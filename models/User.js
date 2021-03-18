96eb77f8d8573a2ae84e176f0e8a24f6cc14c648
BELGUESMI Aymen <77270008+Aymen491@users.noreply.github.com>
2021-03-19 00:20:32 +0100

    Add files via upload

---
+++ models/User.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose')
+
+const Schema =  mongoose.Schema
+
+const UserSchema = new Schema({
+    name: {type: String , required : true} ,
+    age: {type:Number},
+    phone : {type: Number}
+});
+
+
+module.exports  = mongoose.model("User" , UserSchema)
+
+
